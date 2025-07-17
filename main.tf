7dddadbfc0cff656619a94eeba7aaeac1f90a392
suryadwd <surajdwivedi644@gmail.com>
2025-07-17 05:40:41 +0530

    day4 of goal

--- main.tf
+++ main.tf
@@ -8,5 +8,28 @@ module "dev-infra" {
   ami = "ami-014e30c8a36252ae5"
   db_name = "suarj"
   hash_key = "mereId"
+}
 
+module "prod-infra" {
+  source = "./module"
+  env = "prod"
+  bucket_name = "suraj-infra-bucket"
+  sg_name = "suraj"
+  instance_count = 2
+  instance_type = "t2.micro"
+  ami = "ami-014e30c8a36252ae5"
+  db_name = "suarj"
+  hash_key = "mereId"
+}
+
+module "stg-infra" {
+  source = "./module"
+  env = "stg"
+  bucket_name = "suraj-infra-bucket"
+  sg_name = "suraj"
+  instance_count = 1
+  instance_type = "t2.micro"
+  ami = "ami-014e30c8a36252ae5"
+  db_name = "suarj"
+  hash_key = "mereId"
 }
